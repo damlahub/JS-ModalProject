68d09a15abbb35b6736e3e0213f2f01c23df3d4c
DamlaOksel <damlaoksel@hotmail.com>
2023-08-19 14:26:37 +0300

    ModalProject

---
+++ app.js
@@ -0,0 +1,22 @@
+let container = document.querySelector(".container");
+
+function Show() {
+    let div = document.createElement("div");
+    div.classList.add("panel");
+    div.onclick = ClosePanel;
+    div.innerHTML = `
+        <div class="message">
+                <h1>Hi!</h1>
+            </div>
+    `;
+    container.appendChild(div);
+}
+function ClosePanel(e) {
+    let clickClass = e.target.classList;
+    if (clickClass.contains("panel")) {
+        e.target.style.animation = "close-fade 1s ease forwards";
+        e.target.addEventListener("animationend", function () {
+            e.target.remove();
+        });
+    }
+}
